dfae9f0236c8cda89ec63f4c6f386dc7db297022
RaduBalaceanu1 <br.balaceanu.radu@gmail.com>
2024-09-27 23:01:32 +0300

    Initial Commit

---
+++ daydrift/daydriftApp.swift
@@ -0,0 +1,20 @@
+//
+//  daydriftApp.swift
+//  daydrift
+//
+//  Created by Anca Balaceanu on 8/7/24.
+//
+
+import SwiftUI
+
+@main
+struct daydriftApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
